0c5887195882d9a0fb6e88a093dfe6aaad477ada
Fernando Alvarez <fernando.alvarez@lexitaslegal.com>
2025-11-26 00:59:32 -0600

    Add definitions to user

--- src/interfaces/ISecretDitoRepo.py
+++ src/interfaces/ISecretDitoRepo.py
@@ -1,15 +1,17 @@
 from abc import ABC
 from abc import abstractmethod
 
+from models.User import User
+
 class ISecretDitoRepo(ABC):
     @abstractmethod
-    async def getUserById(self, user_id: int):
+    async def getUserById(self, user_id: int) -> User:
         pass
 
     @abstractmethod
-    async def createUser(self, user):
+    async def createUser(self, user: User):
         pass
 
     @abstractmethod
-    async def updateUser(self, user):
+    async def updateUser(self, user: User):
         pass
