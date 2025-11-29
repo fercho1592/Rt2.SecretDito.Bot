2771a1bd5b864d54eda251f6d8aca26386fda34f
Fernando Alvarez <fernando.alvarez@lexitaslegal.com>
2025-11-28 21:14:14 -0600

    update user notification

--- src/run_user_notification.py
+++ src/run_user_notification.py
@@ -10,7 +10,6 @@ async def notify_all_users(botFacade: IBotApiFacade, repo: IAdminSecretDitoRepo)
         await notify_user(botFacade, user)
     pass
 
-
 async def notify_user(botFacade: IBotApiFacade, user: User):
     message = f'Hola {user.name}, ya se te ha asignado un amigo secreto!\n'+\
             'Usa /secret_friend para ver quien es \n'+\
@@ -24,5 +23,5 @@ if __name__ == '__main__':
     repo = getRepoInstance()
     bot_api = getBotApiFacade()
 
-    user = asyncio.run(repo.GetUserById(1345956985))
-    asyncio.run(notify_user(bot_api, user))
+    asyncio.run(notify_all_users(bot_api, adminRepo))
+    pass
