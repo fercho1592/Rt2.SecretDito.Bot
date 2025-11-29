bd170724527fb4fda76b9b75c0f01f4f4c49b0ad
Fernando Alvarez <fernando.alvarez@lexitaslegal.com>
2025-11-28 20:20:28 -0600

    Update how messages is sent

--- src/run_user_notification.py
+++ src/run_user_notification.py
@@ -3,17 +3,20 @@ from dependency import getAdminRepoInstance, getRepoInstance, getBotApiFacade
 from models.User import User
 from interfaces.IBotApiFacade import IBotApiFacade
 from interfaces.IAdminSecretDitoRepo import IAdminSecretDitoRepo
-from interfaces.ISecretDitoRepo import ISecretDitoRepo
 
 async def notify_all_users(botFacade: IBotApiFacade, repo: IAdminSecretDitoRepo):
     users:list[User] = await repo.GetAllUsers()
     for user in users:
-        message = f"Hello {user.name}, this is a notification!"
-        await botFacade.notify_user(user, message)
+        await notify_user(botFacade, user)
+    pass
 
-async def notify_user(botFacade: IBotApiFacade, repo: ISecretDitoRepo):
-    user:User = await repo.GetUserById(1345956985)
-    message = f"Hello {user.name}, this is a notification!"
+
+async def notify_user(botFacade: IBotApiFacade, user: User):
+    message = f'Hola {user.name}, ya se te ha asignado un amigo secreto!\n'+\
+            'Usa /secret_friend para ver quien es \n'+\
+            'Y /secret_wish_list para ver su wish list'
+    await botFacade.notify_user(user, message)
+    message = 'Y recuerda mantener tu wish list actualizada compartiendos por este chat'
     await botFacade.notify_user(user, message)
 
 if __name__ == '__main__':
@@ -21,4 +24,5 @@ if __name__ == '__main__':
     repo = getRepoInstance()
     bot_api = getBotApiFacade()
 
-    asyncio.run(notify_user(bot_api, repo))
+    user = asyncio.run(repo.GetUserById(1345956985))
+    asyncio.run(notify_user(bot_api, user))
