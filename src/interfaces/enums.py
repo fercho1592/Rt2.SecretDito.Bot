949524dcde0eaa9ff96959d908f72a4b6a16ff94
Fernando Alvarez <fernando.alvarez@lexitaslegal.com>
2025-12-15 19:43:56 -0600

    Add a method to get default values for settings

--- src/interfaces/enums.py
+++ src/interfaces/enums.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from typing import Self
+
 
 class ConfigEnum(Enum):
     TELEGRAM_TOKEN = "telegram_token"
@@ -6,6 +8,13 @@ class ConfigEnum(Enum):
     GRAPH_SETTINGS_FILE = "graph_settings_file"
     # Agrega aquí más claves según se necesiten
 
+    @staticmethod
+    def get_default(service_name: Self) -> str:
+        default = {
+            ConfigEnum.ASSIGNATION_FILE: "assignation.json",
+            ConfigEnum.GRAPH_SETTINGS_FILE: "graph_settings.json",
+        }
+        return default.get(service_name)
+
     def __str__(self):
         return self.value
-    
