36ad6e62afb2d72739af12aca21897dda7edeece
Fernando Alvarez <fernando.alvarez@lexitaslegal.com>
2025-12-15 19:43:36 -0600

    Remove get all values from protocol

--- src/integration/YamlConfigService.py
+++ src/integration/YamlConfigService.py
@@ -2,13 +2,12 @@ import yaml
 from interfaces.enums import ConfigEnum
 from interfaces.config_protocols import ConfigServiceProtocol
 
+
 class YamlConfigService(ConfigServiceProtocol):
     def __init__(self, config_path: str = ".env"):
         with open(config_path, "r") as file:
             self.config = yaml.safe_load(file)
 
     def get_config_value(self, service_name: ConfigEnum) -> str:
-        return self.config.get("services", {}).get(str(service_name), "")
-
-    def get_all_services(self) -> dict:
-        return self.config.get("services", {})
+        default_value = ConfigEnum.DEFAULTS.get(service_name)
+        return self.config.get("services", {}).get(str(service_name), default_value)
--- src/interfaces/config_protocols.py
+++ src/interfaces/config_protocols.py
@@ -1,10 +1,12 @@
-from typing  import Protocol
+from typing import Protocol
 from interfaces.enums import ConfigEnum
+
 
 class ConfigServiceProtocol(Protocol):
     def get_config_value(self, service_name: ConfigEnum) -> str: ...
-    def get_all_services(self) -> dict: ...
-    
+
+
 class FlagServiceProtocol(Protocol):
     def is_asignation_done(self) -> bool: ...
+
     pass
